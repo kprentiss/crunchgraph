eb71a7fc99b17d55a0e7699fe79a86acb24df24c
Kevin Prentiss <kevin@redroverhq.com>
2013-06-23 10:29:25 -0400

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crunchgraph::Application.config.secret_token = '2e63e563d29996c79f02c77c3727e3aa09635dd35daa47b361ae8cc54b3f7b6baf9240b2f6e617ddd68153ba2399169f84f9005547599f6dfa05ca319b8f143c'
